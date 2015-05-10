02e2d1244cad52948660607e095a16d5efdccdc6
Lucien Greathouse <me@lpghatguy.com>
2015-05-10 17:47:04 -0600

    Reskin and update!

--- js/common.js
+++ js/common.js
@@ -1,29 +1,69 @@
-ready(function() {
-	// mobile nav
-	var navOut = false;
-	var togglers = document.getElementsByClassName("mobile-nav-toggler");
-	var blackout = document.getElementById("blackout");
-	var mobileMenu = document.getElementsByClassName("mobile-nav")[0];
+(function() {
+	// General utilities
+	function setCookie(name, value) {
+		document.cookie = name + "=" + value;
+	}
 
-	function setElementActive(element, state) {
-		var current = element.className;
-		var cleaned = current.replace(/\bactive\b/i, "");
+	function getCookie(name) {
+		var reg = new RegExp("(?:(?:^|.*;\s*)" + name + "\s*\=\s*([^;]*).*$)|^.*$");
 
-		if (state) {
-			element.className = cleaned + " active";
+		return document.cookie.replace(reg, "$1");
+	}
+
+	// Mobile nav
+
+	ready(function() {
+		var navOut = false;
+		var togglers = document.getElementsByClassName("mobile-nav-toggler");
+		var blackout = document.getElementById("blackout");
+		var mobileMenu = document.getElementsByClassName("mobile-nav")[0];
+
+		function setElementActive(element, state) {
+			var current = element.className;
+			var cleaned = current.replace(/\bactive\b/i, "");
+
+			if (state) {
+				element.className = cleaned + " active";
+			} else {
+				element.className = cleaned;
+			}
+		}
+
+		function toggleNav() {
+			navOut = !navOut;
+
+			setElementActive(mobileMenu, navOut);
+			setElementActive(blackout, navOut);
+		}
+
+		for (var i = 0; i < togglers.length; i++) {
+			togglers[i].addEventListener("click", toggleNav);
+		}
+	});
+
+	// Lights on/off button
+	var lights = document.getElementsByClassName("lights");
+	var sheet = document.querySelector("#theme-sheet");
+	var lightsOn = false;
+
+	function toggleLights() {
+		lightsOn = !lightsOn;
+		setCookie("lights", lightsOn ? "on" : "off");
+
+		if (lightsOn) {
+			sheet.setAttribute("href", "/css/light.css");
 		} else {
-			element.className = cleaned;
+			sheet.setAttribute("href", "/css/dark.css");
 		}
 	}
 
-	function toggleNav() {
-		navOut = !navOut;
-
-		setElementActive(mobileMenu, navOut);
-		setElementActive(blackout, navOut);
+	if (getCookie("lights") == "on") {
+		toggleLights();
 	}
 
-	for (var i = 0; i < togglers.length; i++) {
-		togglers[i].addEventListener("click", toggleNav);
-	}
-});
+	ready(function() {
+		for (var i = 0; i < lights.length; i++) {
+			lights[i].addEventListener("click", toggleLights);
+		}
+	});
+})();
